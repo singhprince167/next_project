5671983b859d6f56bcddbd211aa4394a6df350a5
prince singh <princekumarsingh167@gmail.com>
2024-09-25 14:56:09 +0530

    demo 3 message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Next App",
@@ -14,6 +15,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <div className="absolute left-0 top-10">
+          <ul>
+            <li><Link href="/"> Home</Link></li>
+            <li><Link href="/student"> Student</Link></li>
+            <li><Link href="/student/subject"> Subjects</Link></li>
+          </ul>
+
+        </div>
         {children}
       </body>
     </html>
